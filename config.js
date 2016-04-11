120c63824732eb913e555c5b2274883e873a57fa
Adam Neary <adam@neary.com>
2016-04-10 17:59:29 -0700

    renaming

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 export default {
   firebaseUrl: 'https://shining-fire-9425.firebaseio.com/',
   apiEndpoint: 'https://8hyhfopybl.execute-api.us-west-2.amazonaws.com/dev/todos',
-  awsRegion: 'us-west-2',
-  lamdbaFunctionName: 'todosReducer',
 };
